a180957f06b88b47511ca30356d5bfd33fa086c7
MadHowl <madhowl@yandex.ru>
2025-11-17 15:52:37 +1000

    add Admin template and rout + di for admin

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,11 +1,13 @@
 <?php
 declare(strict_types=1);
 
+use App\Controllers\AdminController;
 use App\Controllers\FrontController;
 use App\Factories\PostFactory;
 use App\Interfaces\PostFactoryInterface;
 use App\Interfaces\PostRepositoryInterface;
 use App\Repositories\JsonPostRepository;
+use App\Views\AdminView;
 use App\Views\FrontView;
 use League\Route\Router;
 use League\Route\Strategy\ApplicationStrategy;
@@ -23,11 +25,17 @@ $container->add(Environment::class, function () {
 
 $container->add(FrontView::class)
     ->addArguments([Environment::class]);
+$container->add(AdminView::class)
+    ->addArguments([Environment::class]);
 // PostFactory
 $container->add(PostFactoryInterface::class, PostFactory::class);
 
 $container->add(PostRepositoryInterface::class, JsonPostRepository::class)
     ->addArguments([PostFactoryInterface::class]);
+
+$container->add(AdminController::class)
+    ->addArgument($container->get(PostRepositoryInterface::class))
+    ->addArgument($container->get(AdminView::class));
 
 $container->add(FrontController::class)
     ->addArgument($container->get(PostRepositoryInterface::class))
